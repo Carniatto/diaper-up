93902ced63d4329494b92817dc0b3b61aeb788d6
Mateus Carniatto <mateus.carniatto@gmail.com>
2024-08-09 10:43:34 +0200

    implement last poo icon

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,6 +6,7 @@ import { UserService } from '../user.service';
 import { copy, link } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
 import { DatePipe } from '@angular/common';
+import { formatDistance, formatRelative } from 'date-fns';
 
 @Component({
   selector: 'app-home',
@@ -36,6 +37,61 @@ export class HomePage {
   lastRoutine = computed(() => {
     return this.routineService.routines().filter(r => r.bottleAmount > 0).at(0);
   });
+
+  lastPoopTime = computed(() => {
+    const lastPoop = this.routineService.routines().filter(r => r.hasPooped).at(0);
+    return {
+      time: lastPoop ? formatDistance(new Date(lastPoop.timestamp), Date.now(),) : 'Never',
+      poopSvg: lastPoop ? this.poopButttons.find(b => b.value === lastPoop.poopColor)?.svg : 'assets/svg/no-poop-black.svg'
+    }
+  });
+
+  poopButttons = [
+    {
+      label: 'No Poop',
+      svg: 'assets/svg/no-poop-black.svg',
+      value: 0,
+      abnormal: false
+    },
+    {
+      label: 'Pale Poop',
+      svg: 'assets/svg/poop-pale-yellow.svg',
+      value: 1,
+      abnormal: true
+    },
+    {
+      label: 'Beige Poop',
+      svg: 'assets/svg/poop-beige.svg',
+      value: 2,
+      abnormal: true
+    },
+    {
+      label: 'White Poop',
+      svg: 'assets/svg/poop-white-putty.svg',
+      value: 3,
+      abnormal: true
+    },
+    {
+      label: 'Gold Poop',
+      svg: 'assets/svg/poop-gold-yellow.svg',
+      value: 4,
+      abnormal: false
+    },
+    {
+      label: 'Bronze Poop',
+      svg: 'assets/svg/poop-bronze-ocre.svg',
+      value: 5,
+      abnormal: false
+    },
+    {
+      label: 'Green Poop',
+      svg: 'assets/svg/poop-green.svg',
+      value: 6,
+      abnormal: false
+    },
+
+
+  ]
 
   constructor() {
     addIcons({ link, copy });
