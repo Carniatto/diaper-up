d98a6fd9f2054cad2a01215d6565f8593d1c8fb3
Mateus Carniatto <mateus.carniatto@gmail.com>
2025-04-02 16:26:03 -0300

    feat(pwa): add service worker and manifest for PWA support

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,58 @@
-import { environment } from './environments/environment';
-import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules, withComponentInputBinding } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { environment } from "./environments/environment";
+import { bootstrapApplication } from "@angular/platform-browser";
+import {
+  RouteReuseStrategy,
+  provideRouter,
+  withPreloading,
+  PreloadAllModules,
+  withComponentInputBinding,
+} from "@angular/router";
+import {
+  IonicRouteStrategy,
+  provideIonicAngular,
+} from "@ionic/angular/standalone";
 
-import { routes } from './app/app.routes';
-import { AppComponent } from './app/app.component';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getAuth, provideAuth } from '@angular/fire/auth';
-import { getDatabase, provideDatabase } from '@angular/fire/database';
-import { connectFirestoreEmulator, getFirestore, provideFirestore } from '@angular/fire/firestore';
-import * as firebaseConfig from '../firebase.json'
+import { routes } from "./app/app.routes";
+import { AppComponent } from "./app/app.component";
+import { getApp, initializeApp, provideFirebaseApp } from "@angular/fire/app";
+import { getAuth, provideAuth } from "@angular/fire/auth";
+import {
+  connectFirestoreEmulator,
+  getFirestore,
+  initializeFirestore,
+  persistentLocalCache,
+  persistentMultipleTabManager,
+  provideFirestore,
+} from "@angular/fire/firestore";
+import * as firebaseConfig from "../firebase.json";
+import { isDevMode } from "@angular/core";
+import { provideServiceWorker } from "@angular/service-worker";
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes, withPreloading(PreloadAllModules), withComponentInputBinding()),
+    provideRouter(
+      routes,
+      withPreloading(PreloadAllModules),
+      withComponentInputBinding()
+    ),
     provideFirebaseApp(() => initializeApp(firebaseConfig)),
     provideAuth(() => getAuth()),
     provideFirestore(() => {
-      const db = getFirestore();
+      const db = initializeFirestore(getApp(), {
+        localCache: persistentLocalCache({
+          tabManager: persistentMultipleTabManager(),
+        }),
+      });
       if (!environment.production) {
-        connectFirestoreEmulator(db, '127.0.0.1', 8080)
+        connectFirestoreEmulator(db, "127.0.0.1", 8080);
       }
       return db;
     }),
+    provideServiceWorker("ngsw-worker.js", {
+      // enabled: !isDevMode(),
+      registrationStrategy: "registerWhenStable:30000",
+    }),
   ],
 });
-
-""
